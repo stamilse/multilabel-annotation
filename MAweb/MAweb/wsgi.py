d93e7fd74661c9d996ecbc570708c0248059afec
Shashank Gupta <35930667+ShshnkGpta@users.noreply.github.com>
2019-06-25 13:08:12 +0530

    Started WebApp development
    Using Django

---
+++ MAweb/MAweb/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for MAweb project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MAweb.settings')
+
+application = get_wsgi_application()
